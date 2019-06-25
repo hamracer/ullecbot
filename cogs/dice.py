b23b08e4b83570dbb72ace84e8adf0a66b9c6486
hamracer <congeegee@gmail.com>
2019-06-26 00:49:24 +1000

    test

--- cogs/dice.py
+++ cogs/dice.py
@@ -18,19 +18,26 @@ class diceCog(commands.Cog, name="dice"):
         resultnum = 0
         output = []
         author = ctx.author.name
-        titlestring = author + " just rolled the dice 🎲"
-        embed = discord.Embed(color=9062d3)
+        titlestring = author + " just rolled the dice 🎲 \n"
+        embed = discord.Embed(color=0x9062d3)
         embed.set_author(name="ullec bot")
         embed.set_thumbnail(url=ctx.author.avatar_url)
         try:
             if len(arg) > 2:
                 raise Exception
             if len(arg) == 1:
-                b = arg[0]
-                b = b.strip("d")
-                b = int(b)
-                result = self.dice(b)
-                rolls = 1
+                try:
+                    b = arg[0]
+                    b = b.strip("d")
+                    b = int(b)
+                    result = self.dice(b)
+                    rolls = 1
+                except:
+                    b = arg[0]
+                    c = b.split("d")
+                    b = int(c[1])
+                    result = self.dice(b)
+                    rolls = int(c[0])
             if len(arg) == 2:
                 for id, i in enumerate(arg):
                     g = arg[id]
@@ -55,11 +62,12 @@ class diceCog(commands.Cog, name="dice"):
                 raise Exception
 
             foutput = '\n'.join(output)
-            embed.add_field(name=titlestring, value=foutput)
-            totalstr = "Total of " + str(resultnum)
-            embed.add_field(name=totalstr, value="\u200b")
+            embed.insert_field_at(index=1, name=titlestring, value=foutput)
+            totalstr = "For a total of " + str(resultnum)
+            embed.insert_field_at(index=5, name=totalstr, value="\u200b", inline=False)
             await ctx.send(embed=embed)
-        except Exception:
+        except Exception as e:
+            print('Exception: ' + str(e)) 
             await ctx.message.add_reaction(emoji=':worst:579662420537114626')
 
     
--- cogs/starchart.py
+++ cogs/starchart.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 starcount = {}
+checklist = []
 channellist = [339155308767215618, 562352225423458326, 262371002577715201]
 
 
@@ -15,20 +16,62 @@ class starchartCog(commands.Cog, name="starchart"):
         if reaction.message.channel.id in channellist: 
                 # check if reaction is a star
             if reaction.emoji == "⭐":
-                react_id = reaction.message.id
-                try:
-                    starcount[react_id] += 1
-                except:
-                    starcount[react_id] = 1
+                react_id = reaction.message.id           
+                print("we see the star")
+                    # for users on the reaction list
+                async for rusers in reaction.users():
+                    try:
+                        print("getting last user")
+                        for i in starcount[react_id]:
+                            if rusers.id is not i:
+                                print("ruser id")
+                                print(rusers.id)
+                                starcount[react_id].append(rusers.id)
+                                print("appending to list")
+                                    # remove dupes coz i dont know why there are dupes im dumb
+                                starcount[react_id] = list(dict.fromkeys(starcount[react_id]))
+                                
+                            else:
+                                print("dont append to list")
+                    # if we don't find a message in the dict created an instance for it
+                    except:
+                        starcount[react_id] = [rusers.id]
+                        print("creating new listing")
+
             print(starcount)
             for i in starcount:
-                if starcount[i] > 4:
-                    nick = reaction.message.author.nick
-                    
+                    # if there are 5 or more stars
+                if len(starcount[i]) == 5:
+                    if react_id not in checklist:
+                        checklist.append(react_id)
+                        nick = reaction.message.author
+                        pfp = reaction.message.author.avatar_url
 
+                            # setup the embed
+                        guild_id = reaction.message.guild.id
+                        channel_id = reaction.message.channel.id
+                        message_id = reaction.message.id
+                        content = reaction.message.content
+                        linkstr = "https://discordapp.com/channels/"+str(guild_id)+"/"+str(channel_id)+"/"+str(message_id)
+                        titlestr = "> ["+str(message_id)+"]("+str(linkstr)+")"
+                        embed = discord.Embed(color=0x9062d3, description=titlestr)
+                        embed.set_thumbnail(url=pfp)
+                        embed.set_author(name=nick)
+                        if content.strip(): 
+                            embed.add_field(name='\u200b', value=content)
 
-
-
+                            # for attachments
+                        if reaction.message.attachments:
+                            file = reaction.message.attachments[0]
+                            if file.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
+                                embed.set_image(url=file.url)
+                            else:
+                                embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
+                        
+                            #find the starboard channel
+                        for channel in reaction.message.guild.channels:
+                            if str(channel) == "starboard":
+                                await channel.send(embed=embed)
 
 
 
