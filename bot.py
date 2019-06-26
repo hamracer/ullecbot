04565006fb1f4900d215f0116ea372ff3a3ea5bf
hamracer <totallynotcellu@gmail.com>
2019-06-27 03:15:27 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,15 +21,13 @@ def loadtoken():
         with open('configs/token.json') as f:
             print('loading token file for main bot')
             data = json.load(f)
-            try:
-                bot_token = data['bot_token']
-            except:
-                bot_token = os.environ.get('bot_token')
+            bot_token = data['bot_token']
+            return True
+    except:
+        bot_token = os.environ.get('bot_token')
         return True
 
-    except Exception as e:
-        print('Exception: ' + str(e))
-        return False
+
 
 if not loadtoken():
     exit()
