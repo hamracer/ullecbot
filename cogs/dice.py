d6a52e5d03e437633e7803a889583b9794555ce6
hamracer <totallynotcellu@gmail.com>
2020-01-28 12:08:55 +1100

    Update dice.py

--- cogs/dice.py
+++ cogs/dice.py
@@ -27,70 +27,73 @@ class diceCog(commands.Cog, name="dice"):
             await ctx.message.add_reaction(emoji=':worst:579662420537114626')
             return
 
-        elif ctx.channel.id in channellist: 
-            r = 0
-            resultnum = 0
-            output = []
-            author = ctx.author.name
-            titlestring = author + " just rolled the dice 🎲 \n"
-            embed = discord.Embed(color=0x9062d3)
-            embed.set_author(name="ullec bot")
-            embed.set_thumbnail(url=ctx.author.avatar_url)
-            try:
-                if len(arg) > 2:
-                    raise Exception
-                if len(arg) == 1:
-                    try:
-                        b = arg[0]
-                        b = b.strip("d")
-                        b = int(b)
-                        result = self.dice(b)
-                        rolls = 1
-                    except:
-                        b = arg[0]
-                        c = b.split("d")
-                        b = int(c[1])
-                        result = self.dice(b)
-                        rolls = int(c[0])
-                if len(arg) == 2:
-                    for id, i in enumerate(arg):
-                        g = arg[id]
-                        if "d" in g:
-                            b = g
+        else:
+            if ctx.channel.id in channellist: 
+                r = 0
+                resultnum = 0
+                output = []
+                author = ctx.author.name
+                titlestring = author + " just rolled the dice 🎲 \n"
+                embed = discord.Embed(color=0x9062d3)
+                embed.set_author(name="ullec bot")
+                embed.set_thumbnail(url=ctx.author.avatar_url)
+                try:
+                    if len(arg) > 2:
+                        raise Exception
+                    if len(arg) == 1:
+                        try:
+                            b = arg[0]
                             b = b.strip("d")
                             b = int(b)
-                            if id == 0:
-                                rolls = int(arg[1])
-                            if id == 1:
-                                rolls = int(arg[0])
-                            
-                        pass
-                while rolls > r:
-                    r += 1
-                    result = self.dice(b)
-                    resultnum = resultnum + int(result)
-                    resultstring = "You rolled "+ result+"/"+str(b)
-                    output.append(resultstring)
-                if b > 100:
-                    raise Exception
-                if rolls > 14:
-                    raise Exception
+                            result = self.dice(b)
+                            rolls = 1
+                        except:
+                            b = arg[0]
+                            c = b.split("d")
+                            b = int(c[1])
+                            result = self.dice(b)
+                            rolls = int(c[0])
+                    if len(arg) == 2:
+                        for id, i in enumerate(arg):
+                            g = arg[id]
+                            if "d" in g:
+                                b = g
+                                b = b.strip("d")
+                                b = int(b)
+                                if id == 0:
+                                    rolls = int(arg[1])
+                                if id == 1:
+                                    rolls = int(arg[0])
 
-                foutput = '\n'.join(output)
-                embed.insert_field_at(index=1, name=titlestring, value=foutput)
-                print(str(resultnum) + " " + str(rolls) + " " + str(b))
-                totalstr = "Total: " + str(resultnum) + " Average: " + str(round(int(resultnum)/int(rolls),2)) + " Percentile: " + str(int(int(resultnum) * 100 / (int(rolls) * int(b))))
-                embed.insert_field_at(index=5, name=totalstr, value="\u200b", inline=False)
-                await ctx.send(embed=embed)
-                rolllist.append(ctx.author.id)
-            except Exception as e:
-                print('Exception: ' + str(e)) 
-                await ctx.message.add_reaction(emoji=':worst:579662420537114626')
+                            pass
+                    while rolls > r:
+                        r += 1
+                        result = self.dice(b)
+                        resultnum = resultnum + int(result)
+                        resultstring = "You rolled "+ result+"/"+str(b)
+                        output.append(resultstring)
+                    if b > 100:
+                        raise Exception
+                    if rolls > 14:
+                        raise Exception
+
+                    foutput = '\n'.join(output)
+                    embed.insert_field_at(index=1, name=titlestring, value=foutput)
+                    print(str(resultnum) + " " + str(rolls) + " " + str(b))
+                    totalstr = "Total: " + str(resultnum) + " Average: " + str(round(int(resultnum)/int(rolls),2)) + " Percentile: " + str(int(int(resultnum) * 100 / (int(rolls) * int(b))))
+                    embed.insert_field_at(index=5, name=totalstr, value="\u200b", inline=False)
+                    await ctx.send(embed=embed)
+                    rolllist.append(ctx.author.id)
+                    
+                except Exception as e:
+                    print('Exception: ' + str(e)) 
+                    await ctx.message.add_reaction(emoji=':worst:579662420537114626')
 
     @tasks.loop(minutes=2)
     async def clear(self):
         global rolllist
-        rolllist=[]    
+        rolllist=[]
+        print("cleared rollist")
 
 def setup(bot):
     bot.add_cog(diceCog(bot))
