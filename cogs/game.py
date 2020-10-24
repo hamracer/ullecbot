fa2583cc3009cf62bcf0c35ffcf47b2129370774
hamracer <totallynotcellu@gmail.com>
2020-10-25 02:08:57 +1100

    yep

--- cogs/game.py
+++ cogs/game.py
@@ -3,7 +3,18 @@ import random
 import sqlite3
 from discord.ext import commands
 
-conn = sqlite3.connect()
+conn = sqlite3.connect('db\game.db')
+c = conn.cursor
+
+#first time run
+
+try:
+    c.execute('''CREATE TABLE players
+                (id, name, hp, str, dex, int, con, wis, cha)''')
+                
+except Exception as e:
+            print('{} cannot be loaded. [{}]'.format(load, e))
+
 
 class gameCog(commands.Cog, name="game"):
     def __init__(self, bot):
