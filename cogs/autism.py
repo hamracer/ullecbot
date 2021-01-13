e125f0ab236cf537b536bd36ab3670cf946673af
Joel <congeegee@gmail.com>
2021-01-13 20:40:26 +1100

    uhhh

--- cogs/autism.py
+++ cogs/autism.py
@@ -11,24 +11,42 @@ class autismCog(commands.Cog, name="autism"):
     @commands.command()
     @commands.has_permissions(manage_roles=True)
     async def autism (self, ctx):
+        userlist=[]
+        count1=0
         conn = sqlite3.connect("configs/autism.db")
         c = conn.cursor()
-        c.execute('''CREATE TABLE autism (user NOT NULL PRIMARY KEY INT, posts INT)''')
-
-        first = datetime.today().replace(day=1)
+        try:
+            c.execute('''CREATE TABLE autism (user_id INTEGER PRIMARY KEY, posts INTEGER)''')
+        except:
+            print("table already exists")
+        first = datetime.today().replace(day=1,hour=0,minute=0,second=0,microsecond=0)
         channel = ctx.channel
-        async for message in channel.history(after=first):
+        print (first)
+        async for message in channel.history(limit=None,after=first):
+            count1 += 1
+            print(count1)
             author = message.author.id
             try:
-                c.execute("""INSERT INTO autism VALUES (?,?)""",(author,int(1)))
+                c.execute("""INSERT INTO autism VALUES (?,?)""",(author,1))
             except:
-                c.execute("""UPDATE austism SET posts = posts + 1 WHERE user = ?""",(author))
+                c.execute("UPDATE autism SET posts = posts + 1 WHERE user_id = ?", (author,))
         conn.commit()
 
-        query = c.execute("SELECT * FROM autism")
-        data = c.fetchall()
+        c.execute("SELECT * FROM autism ORDER BY posts DESC")
+        data = list(c.fetchall())
+        counting=0
         for d in data:
-            print(d)
+            counting+= 1
+            username = ctx.guild.get_member(d[0])
+            stringy = str(username) + ": " + str(d[1])
+            userlist.append(stringy)
+            print(counting)
+        c.execute("DROP TABLE autism")
+        for i in userlist:
+            print(i)
+
+
+
 
 def setup(bot):
     bot.add_cog(autismCog(bot))
