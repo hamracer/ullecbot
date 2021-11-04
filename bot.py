a654711008f7c911aad15d4eebe89aac7f2509d7
hamracer <totallynotcellu@gmail.com>
2021-11-05 02:09:28 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='.', intents=intents)  # bot command
 #coglist = ['dump','snail', 'help', 'dice', 'echo', 'pingstats', 'anime','dnd','uwu','modabuse','muri']
 #coglist = ['echo']
-coglist = ['dump','snail', 'help', 'dice', 'echo', 'pingstats', 'anime','dnd','uwu','modabuse','muri']
+coglist = ['dump','snail', 'help', 'dice', 'pingstats', 'anime','dnd','uwu','modabuse','muri']
 bot.remove_command('help')
 spook = [   "This only makes me stronger.",
             "I’m /gbfg/'s reckoning.",
