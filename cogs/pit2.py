eaaa880f53290cf2cfeccd951d7fc1863c708734
Cellu <totallynotcellu@gmail.com>
2024-03-22 22:05:21 +1100

    mm

--- cogs/pit2.py
+++ cogs/pit2.py
@@ -4,14 +4,36 @@ import random
 import asyncio
 from datetime import datetime,date
 from dateutil.relativedelta import relativedelta
-import math
-from PIL import Image
+import os
+import psycopg
+import psycopg_pool
+from dotenv import load_dotenv
 
+path=str(os.getcwd()) + '/configs/.env'
+load_dotenv(override=True,dotenv_path=path)
+connection_string = os.getenv('DATABASE_URL')
+pool = psycopg_pool.AsyncConnectionPool(connection_string)
 
 class pit2Cog(commands.Cog, name="pit2"):
 
     def __init__(self, bot):
         self.bot = bot
+        
+
+
+    async def openpool(self):
+        try:
+            async with pool.connection() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute('SELECT NOW();')
+                    time = await cur.fetchone()
+                    await cur.execute('SELECT version();')
+                    version = await cur.fetchone()
+                    print('Current time:', time)
+                    print('PostgreSQL version:', version)
+        except:
+            print('something went wrong tehe')
+
 
 
     def getpowerlevel(self, ctx, dodger):
@@ -29,12 +51,36 @@ class pit2Cog(commands.Cog, name="pit2"):
         diceroll = random.randint(1,20)
         return diceroll
 
+
+
+    @commands.command()
+    @commands.has_role("cumdev")
+    async def test(self, ctx):
+        await self.openpool()
+
+    @commands.command()
+    @commands.has_role("cumdev")
+    async def ct(self, ctx):
+        print('1')
+        async with pool.connection() as conn:
+            print('1')
+            async with conn.cursor() as cur:
+                print('1')
+                await cur.execute("CREATE TABLE pit2 (userid integer PRIMARY KEY, hits integer, clears integer, kills integer)")
+                print('1')
+                print("table created")
+
+
+
     @commands.command()
     async def bh(self, ctx):
         print('bullethell')
         dodger = ctx.author
+        userid = ctx.author.id
         dodgerpl = self.getpowerlevel(ctx, dodger)
-
+        #id check
+        
+ 
         modrole = discord.utils.get(ctx.guild.roles, name="mod")
         modnum = [m.name for m in modrole.members]
 
@@ -51,8 +97,6 @@ class pit2Cog(commands.Cog, name="pit2"):
         hitcounter = int(0)
         while rolls > i:
             embed.clear_fields()
-            print("checking power1")
-            print(i)
             i += 1
             print(i)
             # mod powerlevel
@@ -63,19 +107,12 @@ class pit2Cog(commands.Cog, name="pit2"):
             print(str(modpl) + " vs " + str(dodgerpl))
 
             embed.add_field(name=str(i) + " of " + str(rolls), value="", inline=False)
-            print("starting")
             
             if modpl > dodgerpl:
-                print("checking power2")
                 modroll = modroll + 2
-                print("checking power2")
             else:
-                print("checking power3")
                 modroll = modroll - 1
-                print("checking power3")
-            print(str(modroll) + " vs " + str(dodgerac))
             if modroll > dodgerac:
-                print('hit')
                 hitcounter += 1
                 inittitle = str(ctx.author.display_name + ' has been hit ' + str(hitcounter) + " times")
                 embed = discord.Embed(color=0x9062d3, title = inittitle)
@@ -84,17 +121,14 @@ class pit2Cog(commands.Cog, name="pit2"):
                 embed.add_field(name=str(modM[i-1].display_name) + " shoots " + str(dodger), value=str(dodger) + " has been hit!", inline=False)
                 
             else: 
-                print('dodge')
                 inittitle = str(ctx.author.display_name + ' has been hit ' + str(hitcounter) + " times")
                 embed = discord.Embed(color=0x9062d3, title = inittitle)
                 embed.set_thumbnail(url=ctx.author.avatar.url)
                 embed.set_footer(text=str(modroll) + "/20 ≤ " + str(dodgerac))
                 embed.add_field(name=str(modM[i-1].display_name) + " shoots " + str(dodger), value=str(dodger) + " dodges!", inline=False)
-            print("waiting")
 
             await asyncio.sleep(3)
             await emby.edit(embed=embed)
-            print("END------------------------")
 
         await asyncio.sleep(3)
         if hitcounter > 0:
@@ -116,3 +150,5 @@ class pit2Cog(commands.Cog, name="pit2"):
 async def setup(bot):
     await bot.add_cog(pit2Cog(bot))
     print('pit2 cog loaded')
+    
+    
