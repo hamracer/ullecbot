458b972c58bdc14e50152d8767418f807b1b63d8
26cellu <totallynotcellu@gmail.com>
2024-03-27 21:41:30 +1100

    mmm i tihnk ti sifne

---
+++ cogs/countdown.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+import time
+from datetime import datetime
+
+
+
+
+class countdownCog(commands.Cog, name="countdown"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def howlongleftuntiligetonaplaneforjapan(self, ctx):
+        futuredate = datetime.strptime("28/8/2024 11:10:00","%d/%m/%Y %H:%M:%S")
+        nowdate = datetime.now()
+        countdown = futuredate - nowdate
+        s = countdown.total_seconds()
+        seconds_to_minute   = 60
+        seconds_to_hour     = 60 * seconds_to_minute
+        seconds_to_day      = 24 * seconds_to_hour
+
+        days    =   s // seconds_to_day
+        s    %=  seconds_to_day
+
+        hours   =   s // seconds_to_hour
+        s    %=  seconds_to_hour
+
+        minutes =   s // seconds_to_minute
+        s    %=  seconds_to_minute
+
+        seconds = s
+
+        await ctx.reply("%d days, %d hours, %d minutes, %d seconds" % (days, hours, minutes, seconds))
+
+
+
+    
+    
+async def setup(bot):
+    await bot.add_cog(countdownCog(bot))
+    print('countdown cog loaded')
